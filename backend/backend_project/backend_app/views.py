0cc1ccf0c6aa8ecbe1c9324b7c3a5325708902c3
Austin Berry <aberry52098@gmail.com>
2023-10-23 15:27:55 -0400

    create user and url viewsets

--- backend/backend_project/backend_app/views.py
+++ backend/backend_project/backend_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import URL, User
+from .serializers import UrlSerializer, UserSerializer
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('username')
+    serializer_class = UserSerializer
+
+class UrlViewSet(viewsets.ModelViewSet):
+    queryset = URL.objects.all().order_by('title')
+    serializer_class = UrlSerializer
