a65cac5a13e6313b341c7b86e1feceb890667d26
Austin Berry <aberry52098@gmail.com>
2023-10-23 15:26:42 -0400

    add components

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,8 @@
+export const NavBar = () => {
+    return (
+        <>
+            <h1>Nav Bar</h1>
+            <h1>URL Shortener</h1>
+        </>
+    )
+}
---
+++ frontend/src/components/SignUp.jsx
@@ -0,0 +1,14 @@
+export const SignUp = () => {
+    return (
+        <>
+            <form>
+                <h2>Create your account</h2>
+                <input type="text" placeholder="create username" />
+                <br/>
+                <input type="text" placeholder="create password" />
+                <br/>
+                <input type="text" placeholder="validate password" />
+            </form>
+        </>
+    )
+}
