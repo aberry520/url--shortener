0d622af56e9ecf9c5197aaf230b5ad5ac9854a45
Austin Berry <aberry52098@gmail.com>
2023-10-24 12:51:14 -0400

    update token route to use custom encryption token

--- backend/backend_project/urls.py
+++ backend/backend_project/urls.py
@@ -20,6 +20,8 @@ from rest_framework import routers
 from backend_project.backend_app import views
 from rest_framework_simplejwt import views as jwt_views
 
+
+
 router = routers.DefaultRouter()
 router.register(r'url', views.UrlViewSet)
 
@@ -28,7 +30,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('logout/', views.LogoutView.as_view(), name="logout"),
     path('token/', 
-          jwt_views.TokenObtainPairView.as_view(), 
+          views.MyTokenObtainPairView.as_view(), 
           name ='token_obtain_pair'),
     path('token/refresh/', 
           jwt_views.TokenRefreshView.as_view(), 
