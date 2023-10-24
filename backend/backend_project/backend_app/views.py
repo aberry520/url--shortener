8ff147e50e2156dc4c775986d4fd3eb4876ffaa5
Austin Berry <aberry52098@gmail.com>
2023-10-24 12:49:20 -0400

    add MyTokenObtainPairSerializer and MyTokenObtainPairView to customize encryption to return username

--- backend/backend_project/backend_app/views.py
+++ backend/backend_project/backend_app/views.py
@@ -6,6 +6,23 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from .models import URL
 from .serializers import UrlSerializer
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+     serializer_class = MyTokenObtainPairSerializer
+
 class UrlViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
