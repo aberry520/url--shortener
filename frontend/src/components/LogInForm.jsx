47e2dfda56f9f398226446094233762db29fe125
Austin Berry <aberry52098@gmail.com>
2023-10-25 09:31:01 -0400

    add log in form

---
+++ frontend/src/components/LogInForm.jsx
@@ -0,0 +1,12 @@
+export const LogInForm = () => {
+    return (
+        <>
+            <form>
+                <h2>Log In</h2>
+                <input type="text" placeholder="enter username" />
+                <br/>
+                <input type="text" placeholder="enter password" />
+            </form>
+        </>
+    )
+}
